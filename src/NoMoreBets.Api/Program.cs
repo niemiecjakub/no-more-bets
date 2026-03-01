3b89e59c105f9590f8e71bcdf26eb26214e76921
niemiecjakub <kniemiec3@gmail.com>
2026-03-01 18:41:07 +0100

    Added project references to sln

---
+++ src/NoMoreBets.Api/Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapControllers();
+
+app.Run();
--- src/NoMoreBets.Infrastructure/DependencyInjection.cs
+++ src/NoMoreBets.Infrastructure/DependencyInjection.cs
@@ -1,1 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using NoMoreBets.Infrastructure.Persistence;
+
 namespace NoMoreBets.Infrastructure;
+
+public static class DependencyInjection
+{
+  public static IServiceCollection AddInfrastructure(this IServiceCollection services, string connectionString)
+  {
+    // Register DbContext
+    services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+
+    // Register repositories
+    //services.AddScoped<IMatchRepository, MatchRepository>();
+    //services.AddScoped<IBettingRepository, BettingRepository>();
+    //services.AddScoped<IPredictionRepository, PredictionRepository>();
+
+    // Register external providers (scrapers, HTTP clients)
+    // services.AddHttpClient<IFotmobClient, FotmobClient>();
+
+    return services;
+  }
+}
