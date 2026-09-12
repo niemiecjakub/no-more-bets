006cc68888111e07f32f46506be9a37602e5f6f9
niemiecjakub <kniemiec3@gmail.com>
2026-09-12 08:53:16 +0200

    comment

--- backend/src/NoMoreBets.Application/Common/IUnitOfWork.cs
+++ backend/src/NoMoreBets.Application/Common/IUnitOfWork.cs
@@ -2,6 +2,5 @@ namespace NoMoreBets.Application.Common;
 
 public interface IUnitOfWork
 {
-  // ponytail: Application stays EF-free. Delete this and call AppDbContext.SaveChangesAsync if Application ever references EF.
   Task SaveChangesAsync(CancellationToken cancellationToken);
 }
